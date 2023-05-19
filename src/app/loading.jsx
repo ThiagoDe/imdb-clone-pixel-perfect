29f77066346fa0ca978a0b0ce4f9c58696d8db40
ThiagoDe <17tawi@gmail.com>
2023-05-19 15:14:06 -0700

    add card component

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,10 +1,9 @@
-import Image from 'next/image'
 import React from 'react'
 
 export default function loading() {
   return (
     <div className="flex justify-center">
-      <Image className="h-96" src="spinner.svg" alt="loading..." />
+      <img className="h-96" src="/spinner.svg" alt="loading..." />
     </div>
   )
 }
