84b5afcfceeffa559e9ebeacf44ca5b199d65908
ThiagoDe <17tawi@gmail.com>
2023-05-18 20:42:38 -0700

    create the header section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,22 @@
+import Header from '@/components/Header'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'IMDB clone',
-  description: 'This is the IMDB clone website',
+  title: 'IMDb clone',
+  description: 'This is the IMDb clone website',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className} suppressHydrationWarning={true}>
+        
+        <Header />
+        {children}
+      </body>
     </html>
   )
 }
